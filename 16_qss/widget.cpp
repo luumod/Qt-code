750bbf306f5cccac4e220deddf038eaddd36f3b3
ylh <1173012900@qq.com>
2023-07-03 21:16:01 +0800

    add QSS界面美化

---
+++ 16_qss/widget.cpp
@@ -0,0 +1,25 @@
+﻿#include "widget.h"
+#include "./ui_widget.h"
+#include <QFile>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    //直接写
+    //this->setStyleSheet("QPushButton{ color: red;}");
+
+    //外部加载
+    QFile qssFile(":/images/qss/qt.css");
+    if (qssFile.open(QFile::OpenModeFlag::ReadOnly)){
+        this->setStyleSheet(qssFile.readAll());
+    }
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
